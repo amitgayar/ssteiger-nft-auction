44607972e183c947f757c6a205c1396a5d8dc609
amitgayar <gayaramit@gmail.com>
2022-03-29 13:58:38 +0530

    hardhat changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 
-const INFURA_PROJECT_ID = "e51d322e40ac48219cee96531fbd80d5"
-const METAMASK_PRIVATE_KEY = "5f6e3e96f4a1b82065d605833221bd213acf639e4e7d973f466348791a5b09c1" ;
+const INFURA_PROJECT_ID = ""
+const METAMASK_PRIVATE_KEY = "" ;
 
 
 module.exports = {
